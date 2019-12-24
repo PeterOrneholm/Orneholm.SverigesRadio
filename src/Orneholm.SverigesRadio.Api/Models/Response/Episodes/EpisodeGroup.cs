af6005f6d55d62f6b83463c2afb9b8b900c65d45
Peter Örneholm <peter@orneholm.com>
2019-12-24 23:37:49 +0100

    fIX

--- src/Orneholm.SverigesRadio.Api/Models/Response/Episodes/EpisodeGroup.cs
+++ src/Orneholm.SverigesRadio.Api/Models/Response/Episodes/EpisodeGroup.cs
@@ -1,9 +1,11 @@
+using System;
+
 namespace Orneholm.SverigesRadio.Api.Models.Response.Episodes
 {
     public class EpisodeGroup
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Url { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
     }
 }
